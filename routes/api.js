c4aa0c9c1fcde0c1a4e4a7c7641daa1bb9a5d465
Toby Warburton <tobywarburton10@gmail.com>
2023-09-03 13:44:17 -0700

    still missing GET/notes

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,30 @@
-const router = require("express").Router();
+const express = require("express").Router();
 const path = require('path');
-
 const fs = require("fs");
+const generateUniqueId = require('generate-unique-id');
 let db = require("../db/db.json");
-
+const id1 = generateUniqueId();
 
 // GET /api/notes to read the db.json file 
 // and return all saved notes as JSON
-
-
-
+express.get("/notes",(req, res)=>{
+    res.json(db)
+  });
 
 // POST /api/notes to receive a new note to save on request body.
 // Then dd it to the db.json file, then return new note to client
-// give each a unique id when saved (npm packages will do this)
+// give each a unique id when saved 
+// (npm package generateUniqueID will do this)
+express.post("/notes",(req, res) => {
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: id1,
+    }
+    db.push(newNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(db));
+    res.json(db)
+})
 
 
-
-module.exports=router 
+module.exports=express 
--- routes/html.js
+++ routes/html.js
@@ -1,16 +1,14 @@
-const router = require("express").Router();
+const express = require("express").Router();
 const path = require('path');
 
-
-
+// GET * to return the index.html
+express.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
 
 // GET /notes to return notes.html file
+express.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
 
-
-// GET * to return the index.html
-
-
-
-
-
-module.exports=router 
+module.exports=express 
