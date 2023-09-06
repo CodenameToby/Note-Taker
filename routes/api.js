462646c47020046e6d3a90235ec8b8635eae5255
Toby Warburton <tobywarburton10@gmail.com>
2023-09-06 16:10:27 -0700

    save commit

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,31 @@
-const express = require("express").Router();
-const path = require('path');
-const fs = require("fs");
-const generateUniqueId = require('generate-unique-id');
+const app = require("express").Router();
 let db = require("../db/db.json");
-const id1 = generateUniqueId();
+const fs = require("fs");
+
+const generateUniqueId = require('generate-unique-id');
+const id2 = generateUniqueId({
+    length: 6,
+    useLetters: false
+  });
 
 // GET /api/notes to read the db.json file 
 // and return all saved notes as JSON
-express.get("/notes",(req, res)=>{
+app.get("/api/notes",(req, res)=>{
     db = JSON.parse(fs.readFileSync(".db/db.json")) || []
-    res.json(db);
+    res.json(db)
   });
 
 // POST /api/notes to receive a new note to save on request body.
 // Then dd it to the db.json file, then return new note to client
 // give each a unique id when saved 
 // (npm package generateUniqueID will do this)
-express.post("/notes",(req, res) => {
+app.post("/api/notes",(req, res) => {
     let newNote = {
         title: req.body.title,
         text: req.body.text,
         id: id1,
     }
-    db.push(newNote);
+    db.push(newNote)
     fs.writeFileSync("./db/db.json", JSON.stringify(db),function(err){
         if(err) throw err 
     })
@@ -30,4 +33,4 @@ express.post("/notes",(req, res) => {
 });
 
 
-module.exports=express 
+module.exports=app 
--- routes/html.js
+++ routes/html.js
@@ -1,14 +1,14 @@
-const express = require("express").Router();
+const app = require("express").Router();
 const path = require('path');
 
 // GET * to return the index.html
-express.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  );
 
 // GET /notes to return notes.html file
-express.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-  });
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+  );
 
-module.exports=express 
+module.exports=app 
